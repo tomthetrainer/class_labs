3098de9b17ff75f7d255f51de1f2aab5383791b5
turambar <turinturumarth@gmail.com>
2017-06-21 13:29:10 -0700

    Add physionet data directly to repository. Also, each subdirectory is in a separate archive now.

---
+++ extract_physionet2012_data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+tar xzvf src/main/resources/physionet2012/sequence.tar.gz --directory src/main/resources/physionet2012
+tar xzvf src/main/resources/physionet2012/resampled.tar.gz --directory src/main/resources/physionet2012
+tar xzvf src/main/resources/physionet2012/mortality.tar.gz --directory src/main/resources/physionet2012
--- get_physionet2012_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://skymindacademy.blob.core.windows.net/physionet2012/physionet2012.tar.gz
-tar xzvf physionet2012.tar.gz
-mv -i physionet2012 src/main/resources/physionet2012
